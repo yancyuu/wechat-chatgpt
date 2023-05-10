cf2cc18ab2b7d03dbfc4f233b5c03a04bada2fd9
yancyyu <1191500820@qq.com>
2023-05-10 18:45:41 +0800

    祈福

--- message/index.ts
+++ message/index.ts
@@ -75,7 +75,7 @@ ${answer}`
         const { FileBox }  = require('file-box')
         const prompts = text.split(",")
         const base64Str = await getImage(prompts)
-        answer = FileBox.FromBase64(base64Str);
+        answer = FileBox.fromBase64(base64Str);
       }
       return answer
     },
